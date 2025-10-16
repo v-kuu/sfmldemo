f3b469e6be5c93625127351a882b895f077c0dbe
Ville Kuusela <vkuusela@student.hive.fi>
2025-10-16 15:53:55 +0300

    Initial DualComplex implementation

---
+++ src/DualComplex.cpp
@@ -0,0 +1,58 @@
+#include "DualComplex.hpp"
+
+DualComplex::DualComplex(std::complex<float> r, std::complex<float> d)
+	: real(r), dual(d)
+{
+}
+
+DualComplex DualComplex::operator+(const DualComplex &other) const
+{
+	return {real + other.real, dual + other.dual};
+}
+
+DualComplex DualComplex::operator-(const DualComplex &other) const
+{
+	return {real - other.real, dual - other.dual};
+}
+
+DualComplex DualComplex::operator*(const DualComplex &other) const
+{
+	return {real * other.real, real * other.dual + dual * other.real};
+}
+
+DualComplex DualComplex::operator*(float scalar) const
+{
+	return {real * scalar, dual * scalar};
+}
+
+bool DualComplex::operator==(const DualComplex &other) const
+{
+	return (real == other.real && dual == other.dual);
+}
+
+bool DualComplex::operator!=(const DualComplex &other) const
+{
+	return (real != other.real || dual != other.dual);
+}
+
+DualComplex DualComplex::normalize(void) const
+{
+	float norm = std::abs(real);
+	if (norm != 0)
+		return {real / norm, dual / norm};
+	return (*this);
+}
+
+sf::Transform DualComplex::toMat3(void) const
+{
+	return {real.real(), -real.imag(), dual.real(),
+			real.imag(), real.real(), dual.imag(),
+			0.0, 0.0, 1.0};
+}
+
+std::ostream &operator<<(std::ostream &os, const DualComplex &dc)
+{
+	os << "{(" << dc.real.real() << " + " << dc.real.imag() << "ε) + ("
+		<< dc.dual.real() << " + " << dc.dual.imag() << "ε)i";
+	return (os);
+}
---
+++ src/DualComplex.hpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics/Transform.hpp>
+#include <complex>
+#include <iostream>
+
+struct	DualComplex
+{
+	std::complex<float> real;
+	std::complex<float> dual;
+
+	DualComplex(std::complex<float> r = {}, std::complex<float> d = {});
+
+	DualComplex operator+(const DualComplex &other) const;
+	DualComplex operator-(const DualComplex &other) const;
+	DualComplex operator*(const DualComplex &other) const;
+	DualComplex operator*(float scalar) const;
+	bool operator==(const DualComplex &other) const;
+	bool operator!=(const DualComplex &other) const;
+
+	DualComplex normalize(void) const;
+	sf::Transform toMat3(void) const;
+};
+
+std::ostream &operator<<(std::ostream &os, const DualComplex &dc);
